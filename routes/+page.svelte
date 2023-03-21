71d3768f799936053ae7aed76727b6a38307643d
MohamedHamdy28 <71794972+MohamedHamdy28@users.noreply.github.com>
2023-03-21 14:16:57 +0000

    Deploying to gh-pages from @ MohamedHamdy28/MohamedHamdy28.github.io@191f3ec654aaf61d6a33d94d8bc0b61b9698666e 🚀

---
+++ routes/+page.svelte
@@ -0,0 +1,20 @@
+<div class="home">
+	<h1>Welcome to my website</h1>
+	<p>
+		Welcome to my personal website! This site is dedicated to providing information about me and the
+		projects I have completed. Here, you can find out more about my background, interests, and
+		experience. Whether you're a potential employer, colleague, or just someone curious about my
+		work, I invite you to explore the site and learn more about me and my passions. From my
+		portfolio to my blog, you'll find a range of content that showcases my skills and expertise.
+		Thank you for visiting, and I look forward to connecting with you soon!
+	</p>
+</div>
+
+<style>
+	.home {
+		font-family: 'Times New Roman', Times, serif;
+		font-size: large;
+		margin-right: 100px;
+		margin-left: 100px;
+	}
+</style>
